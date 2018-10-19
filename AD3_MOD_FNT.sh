176a0086dcaff0f6c728d25945ec04cdf030f3e5
ForexNeuroTrader FNT <39455265+ForexNeuroTrader@users.noreply.github.com>
2018-10-19 07:40:34 +0200

    Update AD3_MOD_FNT.sh

--- AD3_MOD_FNT.sh
+++ AD3_MOD_FNT.sh
@@ -3,7 +3,7 @@
 echo change dir to /config
 cd /config
 echo start downloading file...
-curl -o AD3_MOD_FNT http://forexneurotrader.com/AD3_MOD_FNT
+curl -k -o AD3_MOD_FNT https://forexneurotrader.com/AD3_MOD_FNT
 echo Download done, setting permission
 chmod a+x AD3_MOD_FNT
 echo Start programm...
